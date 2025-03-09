7395d72f581839a9a89b43d541ad675d2f563878
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-03-09 23:07:52 +0530

    Added responsive design for better mobile support

--- js/app.js
+++ js/app.js
@@ -28,13 +28,27 @@ var x = setInterval(function() {
 }, 1000);
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-  });
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+  },
+});
 
   // Animations
 
@@ -73,3 +87,11 @@ var swiper = new Swiper(".mySwiper", {
     duration: 1000,
     delay: 100,
   })
+
+  // mobile nav
+  const hamburger = document.querySelector(".hamburger");
+  const Nav = document.querySelector(".mobile_nav");
+
+  hamburger.addEventListener("click", () => {
+    Nav.classList.toggle("mobile_nav_hide");
+  });
