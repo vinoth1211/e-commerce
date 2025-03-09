a47a762e7f6e39c7c117eeea3b5bf52772418832
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-03-09 20:29:09 +0530

    Add animations for the website

--- js/app.js
+++ js/app.js
@@ -35,3 +35,41 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+  // Animations
+
+  ScrollReveal().reveal(".top_nav",{
+    origin:"bottom",
+    distance:"20px",
+    opacity:0,
+  })
+
+  ScrollReveal().reveal(".nav",{
+    origin:"bottom",
+    distance:"20px",
+    opacity:0,
+    delay: 100,
+  })
+
+  ScrollReveal().reveal(".header",{
+    origin:"bottom",
+    distance:"20px",
+    opacity:0,
+    delay: 200,
+  })
+
+  ScrollReveal().reveal(".section",{
+    origin:"bottom",
+    distance:"20px",
+    opacity:0,
+    duration: 1000,
+    delay: 100,
+  })
+
+  ScrollReveal().reveal(".footer",{
+    origin:"bottom",
+    distance:"20px",
+    opacity:0,
+    duration: 1000,
+    delay: 100,
+  })
